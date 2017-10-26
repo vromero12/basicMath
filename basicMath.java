da2c4fa28d189572f89c6bc12767860bdee5b114
mromero3 <32310506+mromero3@users.noreply.github.com>
2017-10-26 15:57:28 -0400

    added Factor of 3

--- basicMath.java
+++ basicMath.java
@@ -1,6 +1,13 @@
 package basicMath;
 
 public class basicMath {
+	
+	public static boolean factor_3(int i) {
+		if (i % 3 == 0)
+			return true; 
+		
+			return false;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
