84f69e46590dde5ed8d193332c084139e1e7b4f1
vromero12 <vromero@mcbain.org>
2017-10-26 15:57:02 -0400

    regitron
    regitron changes
    
    added is odd function

--- basicMath.java
+++ basicMath.java
@@ -1,10 +1,16 @@
 package basicMath;
 
 public class basicMath {
-
+	private static boolean isOdd(int i) {
+		if(i % 2==0) {
+			return false;
+		}
+		if (i % 2==1)
+			return true;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 }
