6c0ab88b9f1c2fa49f936d0228ee5b72acf35ac3
Reggie McLean <reggieman00@gmail.com>
2017-10-26 15:56:53 -0400

    added factorial function

--- basicMath.java
+++ basicMath.java
@@ -2,6 +2,11 @@ package basicMath;
 
 public class basicMath {
 
+	public static int factorial(int i) {
+		if(i==1)
+			return 1;
+		return i*factorial(i-1);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
